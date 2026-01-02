5c7bc2361eae8286c6b37eac3f31a811ef59551f
LamaSAZ <108194917+LamaSAZ@users.noreply.github.com>
2026-01-02 16:41:29 +0300

    commit initial files

---
+++ index.js
@@ -0,0 +1,23 @@
+let homeID = document.getElementById("home")
+let guestID = document.getElementById("guest")
+
+function addHome1(){
+  homeID.textContent = Number(homeID.textContent) + 1
+  
+}
+function addHome2(){
+  homeID.textContent = Number(homeID.textContent) + 2
+}
+function addHome3(){
+  homeID.textContent = Number(homeID.textContent) + 3
+}
+
+function addGuest1(){
+  guestID.textContent = Number(guestID.textContent) + 1
+}
+function addGuest2(){
+  guestID.textContent = Number(guestID.textContent) + 2
+}
+function addGuest3(){
+  guestID.textContent = Number(guestID.textContent) + 3
+}
