58d0439c38600c4db35b8016becb5626095e1316
LamaSAZ <108194917+LamaSAZ@users.noreply.github.com>
2026-01-02 16:53:17 +0300

    change header reading
    change header reading from "guest" to "away"

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let homeID = document.getElementById("home")
-let guestID = document.getElementById("guest")
+let AwayID = document.getElementById("Away")
 
 function addHome1(){
   homeID.textContent = Number(homeID.textContent) + 1
@@ -12,12 +12,12 @@ function addHome3(){
   homeID.textContent = Number(homeID.textContent) + 3
 }
 
-function addGuest1(){
-  guestID.textContent = Number(guestID.textContent) + 1
+function addAway1(){
+  AwayID.textContent = Number(AwayID.textContent) + 1
 }
-function addGuest2(){
-  guestID.textContent = Number(guestID.textContent) + 2
+function addAway2(){
+  AwayID.textContent = Number(AwayID.textContent) + 2
 }
-function addGuest3(){
-  guestID.textContent = Number(guestID.textContent) + 3
+function addAway3(){
+  AwayID.textContent = Number(AwayID.textContent) + 3
 }
